28204e167ea575bc7c15f763d1865ff7b0e2c884
Nikshay <nikshay.p.jain@gmail.com>
2025-08-05 06:43:22 +0530

    Select best image

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from group import group_similar_images
+from best import pick_best_image_per_folder
 import time
 
 if __name__ == '__main__':
     input_dir = r"C:\Users\niksh\Desktop\pics"
     t1 = time.time()
     group_similar_images(input_dir)
+    pick_best_image_per_folder(input_dir)
     print(f"Time taken: {time.time() - t1:.2f} seconds")
